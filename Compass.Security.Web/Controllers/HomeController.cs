7f277cce8e46263122ad544aae766d00158019a1
Jose Luis Oshiro Gushiken <joshirog@gmail.com>
2021-09-25 21:51:28 -0500

    Test:publish

--- Compass.Security.Web/Controllers/HomeController.cs
+++ Compass.Security.Web/Controllers/HomeController.cs
@@ -33,5 +33,6 @@ namespace Compass.Security.Web.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        
     }
 }
